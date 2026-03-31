afb7ba75b69c6c53cc7aa6e5e346c9c357f55c3e
02Tirtha <tjhaveri99@gmail.com>
2026-03-31 11:51:24 +0530

    cross origin

--- src/main/java/com/tirtha/sfd/config/CorsConfig.java
+++ src/main/java/com/tirtha/sfd/config/CorsConfig.java
@@ -8,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
+   @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://failuredetectionsystem-frontend.onrender.com")
+                .allowedOrigins(
+                        "https://failuredetectionsystem-frontend.onrender.com",
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173",
+                        "http://localhost:3000",
+                        "http://127.0.0.1:3000"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
